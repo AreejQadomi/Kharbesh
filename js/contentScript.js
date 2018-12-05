e2c7833884f23b3f5db6c5e292fc545ea940ff19
arahmad <arahmad@atypon.com>
2018-12-05 11:26:14 +0200

    Style changes on the popup page

--- js/contentScript.js
+++ js/contentScript.js
@@ -3,8 +3,15 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         switch (request.callFunction) {
             case "highlightSelection":
-                highlightSelection();
+                if (highlightSelection()) {
+                    //deselect text after highlighting it
+                    window.getSelection().removeAllRanges();
+                    sendResponse({msg: "highlighted"});
+                } else {
+                    sendResponse({msg: "noSelection"});
+                }
                 break;
+
             case "showPencil":
                 sendResponse({msg: "soon :)"});
                 break;
@@ -12,13 +19,19 @@ chrome.runtime.onMessage.addListener(
     });
 
 function highlightSelection() {
-    let selectionRange = window.getSelection().getRangeAt(0);
-    //Please refer to https://stackoverflow.com/a/12823606 to get safe ranges for the word
-    //let safeRanges = getSafeRanges(selectionRange);
-    // for (let i = 0; i < safeRanges.length; i++) {
-    //     highlightRange(safeRanges[i]);
-    // }
-    highlightRange(selectionRange);
+    if (window.getSelection().toString()) {
+        let selectionRange = window.getSelection().getRangeAt(0);
+        //Please refer to https://stackoverflow.com/a/12823606 - get safe ranges for the word
+        //Error: Failed to execute 'surroundContents' on 'Range': The Range has partially selected a non-Text node.
+        //Discover more later
+        let safeRanges = getSafeRanges(selectionRange);
+        for (let i = 0; i < safeRanges.length; i++) {
+            highlightRange(safeRanges[i]);
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function highlightRange(range) {
@@ -26,4 +39,66 @@ function highlightRange(range) {
     highlightNode.style.backgroundColor = '#FFFF00';
     highlightNode.style.display = 'inline';
     range.surroundContents(highlightNode);
+}
+
+function getSafeRanges(dangerous) {
+    let xm = document.createRange();
+    let a = dangerous.commonAncestorContainer;
+    // Starts -- Work inward from the start, selecting the largest safe range
+    let s = new Array(0), rs = new Array(0);
+    if (dangerous.startContainer !== a)
+        for (let i = dangerous.startContainer; i !== a; i = i.parentNode)
+            s.push(i)
+            ;
+    if (0 < s.length) for (let i = 0; i < s.length; i++) {
+        let xs = document.createRange();
+        if (i) {
+            xs.setStartAfter(s[i - 1]);
+            xs.setEndAfter(s[i].lastChild);
+        }
+        else {
+            xs.setStart(s[i], dangerous.startOffset);
+            xs.setEndAfter(
+                (s[i].nodeType === Node.TEXT_NODE)
+                    ? s[i] : s[i].lastChild
+            );
+        }
+        rs.push(xs);
+    }
+
+    // Ends -- basically the same code reversed
+    let e = new Array(0), re = new Array(0);
+    if (dangerous.endContainer !== a)
+        for (let i = dangerous.endContainer; i !== a; i = i.parentNode)
+            e.push(i)
+            ;
+    if (0 < e.length) for (let i = 0; i < e.length; i++) {
+        let xe = document.createRange();
+        if (i) {
+            xe.setStartBefore(e[i].firstChild);
+            xe.setEndBefore(e[i - 1]);
+        }
+        else {
+            xe.setStartBefore(
+                (e[i].nodeType === Node.TEXT_NODE)
+                    ? e[i] : e[i].firstChild
+            );
+            xe.setEnd(e[i], dangerous.endOffset);
+        }
+        re.unshift(xe);
+    }
+
+    // Middle -- the uncaptured middle
+    if ((0 < s.length) && (0 < e.length)) {
+        xm.setStartAfter(s[s.length - 1]);
+        xm.setEndBefore(e[e.length - 1]);
+    }
+    else {
+        return [dangerous];
+    }
+
+    // Concat
+    rs.push(xm);
+    // Send to Console
+    return rs.concat(re);
 }
--- js/popup.js
+++ js/popup.js
@@ -10,7 +10,12 @@ $(function () {
     $("#highlighter").click(function () {
         //send message to the injected script to execute the highlight function
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {callFunction: 'highlightSelection'});
+            chrome.tabs.sendMessage(tabs[0].id, {callFunction: 'highlightSelection'}, function (response) {
+
+                window.close();
+                if (response.msg.toString() === "noSelection")
+                    alert("Please select a text to highlight!");
+            });
         });
     });
 
@@ -18,6 +23,7 @@ $(function () {
         //send message to the injected script to execute the show pencil function
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {callFunction: 'showPencil'}, function (response) {
+                window.close();
                 alert(response.msg);
             });
         });
